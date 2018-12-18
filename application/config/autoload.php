57628260677a4c51c254d57c3ca779a37613593b
IndahUtamiIlyas <45946053+IndahUtamiIlyas@users.noreply.github.com>
2018-12-19 00:35:09 +0700

    autoload

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('LoginModel','ProsedurModel','KebutuhanModel','LokasiModel','PengirimanModel','RegisterModel');
+$autoload['model'] = array('LoginModel','ProsedurModel','KebutuhanModel','LokasiModel','PengirimanModel','RegisterModel','FeedbackModel');
